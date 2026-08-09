cdd24a5192a090118db02edf30a3787c002d327c
Swetank Kumar <swetankkumar15f@gmail.com>
2026-08-09 13:38:06 +0530

    Nested pair

--- pairs.cpp
+++ pairs.cpp
@@ -1,5 +1,5 @@
 // Simple pair
-
+/*
 #include <iostream>
 using namespace std;
 
@@ -12,6 +12,26 @@ int main()
 
     cout << "First = " << p.first << endl;
     cout << "Second = " << p.second << endl;
+
+    return 0;
+}
+*/
+
+// Nested pair 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    pair<int, pair<int, int>> p;
+
+    cout << "Enter three numbers: ";
+    cin >> p.first >> p.second.first >> p.second.second;
+
+    cout << "First = " << p.first << endl;
+    cout << "Second First = " << p.second.first << endl;
+    cout << "Second Second = " << p.second.second << endl;
 
     return 0;
 }

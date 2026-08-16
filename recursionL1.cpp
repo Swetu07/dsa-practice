9c5856c3f51b317e08d37f795577c285df426456
Swetank Kumar <swetankkumar15f@gmail.com>
2026-08-17 02:15:57 +0530

    Basic Recursion

--- recursionL1.cpp
+++ recursionL1.cpp
@@ -1,4 +1,6 @@
-#include <iostream>
+// N Times Printing
+/*
+include <iostream>
 using namespace std;
 
 void call(int i, int n)
@@ -13,5 +15,27 @@ int main()
     cout<< "Enter n: ";
     cin>> n;
     call(1, n);
+    return 0;
+}
+*/
+
+//Printing Numbers
+#include <iostream>
+using namespace std;
+
+void print( int i, int n){
+    if (i== n) return;
+
+    cout<<i<<" ";
+    print(i+1, n);
+}
+int main()
+{
+    int n;
+    cout<<"Enter N: ";
+    cin>> n;
+
+    print(0,n);
+
     return 0;
 }

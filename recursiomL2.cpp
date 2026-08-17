d6eeabec9219512253458506f81922046eb92309
Swetank Kumar <swetankkumar15f@gmail.com>
2026-08-18 02:06:05 +0530

    N to 1 Backtracking

---
+++ recursiomL2.cpp
@@ -0,0 +1,18 @@
+// N to 1 using the backtracking
+
+#include <iostream>
+using namespace std;
+void print(int i,int n){
+    if(i>n) return;
+    print (i+1, n);
+    cout<< i << " ";
+}
+int main()
+{
+    int n;
+    cout<< "enter N: ";
+    cin>> n;
+    print(1,n);
+
+    return 0;
+}

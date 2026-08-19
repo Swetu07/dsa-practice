976267c8b00dfb6520b30c8a57a7595d3d80de77
Swetank Kumar <swetankkumar15f@gmail.com>
2026-08-20 00:46:16 +0530

    Swap using 2 parameters

--- recursionL4.cpp
+++ recursionL4.cpp
@@ -1,5 +1,5 @@
 // Swap array using Single parameter 
-
+/*
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -17,6 +17,29 @@ int main()
     for (int i = 0; i < n; i++) cin>> arr[i];
     f(0,arr,n);
     for (int i = 0; i < n; i++) cout<< arr[i]<< " ";
+
+    return 0;
+}
+*/
+
+// Swap array using 2 parameter
+#include <bits/stdc++.h>
+using namespace std;
+
+void f(int l, int r, int arr[]) {
+    if(l>=r) return;
+    swap(arr[l], arr[r]);
+    f(l+1, r-1, arr);
+}
+int main()
+{
+    int n;
+    cout<< "Enter N: ";
+    cin>> n;
+    int arr[n];
+    for(int i=0; i<n; i++) cin>>arr[i];
+    f(0, n-1, arr);
+    for(int i=0; i<n; i++) cout<<arr[i] <<" ";
 
     return 0;
 }

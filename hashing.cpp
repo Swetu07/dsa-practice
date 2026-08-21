63a2e3597e34ccf7bfb7df90751ef48489d759e6
Swetank Kumar <swetankkumar15f@gmail.com>
2026-08-21 15:35:19 +0530

    Char Hashing

--- hashing.cpp
+++ hashing.cpp
@@ -1,5 +1,5 @@
 // Number Hashing
-
+/*
 #include <iostream>
 using namespace std;
 
@@ -41,5 +41,32 @@ int main()
         cout << "Frequency = " << hash[number] << endl;
     }
 
+    return 0;
+}
+*/
+
+// Char Hashing
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin>> s;
+    int hash[256] = {0};
+    for ( int i = 0; i < s.size(); i++)
+    {
+        hash[s[i]]++;
+    }
+    int q;
+    cin>> q;
+    while (q--)
+    {
+        char c;
+        cin >> c;
+        cout << hash[c] << endl;
+    }
+    
     return 0;
 }

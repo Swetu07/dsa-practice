cf16b7e0777fc81ab67247a03c86813c786538d2
Swetank Kumar <swetankkumar15f@gmail.com>
2026-08-21 12:45:39 +0530

    Multiple Recursion

---
+++ recursionL5.cpp
@@ -0,0 +1,19 @@
+// fibonacci using multiple recursion
+#include <bits/stdc++.h>
+using namespace std;
+
+int f(int n){
+    if(n<=1) return n;
+    int last= f(n-1);
+    int slast = f(n-2);
+    return last + slast;
+}
+int main()
+{
+    int n;
+    cout << "Enter N: ";
+    cin >> n;
+
+    cout<<f(n);
+    return 0;
+}

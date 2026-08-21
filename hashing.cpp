a4d9e13b9863ecbbe44cbececeb6cdb315cecb68
Swetank Kumar <swetankkumar15f@gmail.com>
2026-08-21 14:43:54 +0530

    Number Hashing

---
+++ hashing.cpp
@@ -0,0 +1,45 @@
+// Number Hashing
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Enter the size: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter the elements: ";
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int hash[13] = {0};
+
+    for (int i = 0; i < n; i++)
+    {
+        hash[arr[i]]++;
+    }
+
+    int q;
+
+    cout << "Enter number of queries: ";
+    cin >> q;
+
+    while (q--)
+    {
+        int number;
+
+        cout << "Enter number: ";
+        cin >> number;
+
+        cout << "Frequency = " << hash[number] << endl;
+    }
+
+    return 0;
+}

7b3d68b0b5d3aac3e29129c386abd30eccb3e125
Swetank Kumar <swetankkumar15f@gmail.com>
2026-08-22 00:48:30 +0530

    Counting Frequencies of Array Elements using unordered map

--- Counting_Frequencies_of_Array_Elements.cpp
+++ Counting_Frequencies_of_Array_Elements.cpp
@@ -1,4 +1,5 @@
 // Counting Frequencies of Array Elements
+/*
 #include <iostream>
 using namespace std;
 
@@ -30,6 +31,38 @@ int main()
         {
             cout << i << " -> " << hash[i] << endl;
         }
+    }
+
+    return 0;
+}
+*/
+
+// Counting Frequencies of Array Elements using unordered map
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    unordered_map<int, int> mp;
+
+    for (int i = 0; i < n; i++)
+    {
+        mp[arr[i]]++;
+    }
+
+    for (auto it : mp)
+    {
+        cout << it.first << " -> " << it.second << endl;
     }
 
     return 0;

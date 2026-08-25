47a94fcd554db66c57a0ea98a3fab19cbd1c877c
Swetank Kumar <swetankkumar15f@gmail.com>
2026-08-25 22:27:03 +0530

    Bubble Sort for best case

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -1,5 +1,5 @@
-// bubble sort for worst and average case
-
+// Bubble sort for worst and average case
+/*
 #include <iostream>
 using namespace std;
 
@@ -27,6 +27,56 @@ int main()
             {
                 swap(arr[j], arr[j + 1]);
             }
+        }
+    }
+
+    cout << "Sorted array: ";
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
+*/
+
+// Bubble sort for best case
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Enter size: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter elements: ";
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        bool swapped = false;
+
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (swapped == false)
+        {
+            break;
         }
     }
 

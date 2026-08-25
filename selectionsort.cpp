4069bbd814799e25b47237ff3e79dbddf6a7e53b
Swetank Kumar <swetankkumar15f@gmail.com>
2026-08-25 21:45:37 +0530

    Selection sort

--- selectionsort.cpp
+++ selectionsort.cpp
@@ -41,3 +41,18 @@ int main()
 
     return 0;
 }
+
+// Another way using this function and calling it
+/*
+void selectionSort (int arr[] , int n){
+    for(int i=0; i<=n-2; i++) {
+    int min = 1;
+    for(int j=0; j<=n-1; j++){
+    if(arr[i] < arr[min]) min=j;
+      }
+      int temp = arr[min];
+      arr[min] = arr[i];
+      arr[i] = temp;
+    }
+}
+*/

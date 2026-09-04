301ff57cd5914d5dc1bf8d85549e8f1abdcebab6
Swetank Kumar <swetankkumar15f@gmail.com>
2026-09-04 22:41:55 +0530

    Check IF array is sorted in increasing order

--- arrayeasyl1.cpp
+++ arrayeasyl1.cpp
@@ -25,6 +25,61 @@ int main()
 */
 
 // 2nd smallest/largest number in the array 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     cout << "Enter N: ";
+//     cin >> n;
+
+//     int arr[n];
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
+
+//     int largest = arr[0];
+//     int Slargest = -1;
+
+//     int smallest = arr[0];
+//     int Ssmallest = INT_MAX;
+
+//     for (int i = 1; i < n; i++)
+//     {
+//         if (arr[i] > largest)
+//         {
+//             Slargest = largest;
+//             largest = arr[i];
+//         }
+//         else if (arr[i] < largest && arr[i] > Slargest)
+//         {
+//             Slargest = arr[i];
+//         }
+//     }
+
+//     for (int i = 1; i < n; i++)
+//     {
+//         if (arr[i] < smallest)
+//         {
+//             Ssmallest = smallest;
+//             smallest = arr[i];
+//         }
+//         else if (arr[i] != smallest && arr[i] < Ssmallest)
+//         {
+//             Ssmallest = arr[i];
+//         }
+//     }
+
+//     cout << "Second Largest : " << Slargest << endl;
+//     cout << "Second Smallest : " << Ssmallest << endl;
+
+//     return 0;
+// }
+
+// check if array sorted 
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -41,40 +96,21 @@ int main()
         cin >> arr[i];
     }
 
-    int largest = arr[0];
-    int Slargest = -1;
-
-    int smallest = arr[0];
-    int Ssmallest = INT_MAX;
+    bool sorted = true;
 
     for (int i = 1; i < n; i++)
     {
-        if (arr[i] > largest)
+        if (arr[i] < arr[i - 1])
         {
-            Slargest = largest;
-            largest = arr[i];
-        }
-        else if (arr[i] < largest && arr[i] > Slargest)
-        {
-            Slargest = arr[i];
+            sorted = false;
+            break;
         }
     }
 
-    for (int i = 1; i < n; i++)
-    {
-        if (arr[i] < smallest)
-        {
-            Ssmallest = smallest;
-            smallest = arr[i];
-        }
-        else if (arr[i] != smallest && arr[i] < Ssmallest)
-        {
-            Ssmallest = arr[i];
-        }
-    }
-
-    cout << "Second Largest : " << Slargest << endl;
-    cout << "Second Smallest : " << Ssmallest << endl;
+    if (sorted)
+        cout << "Array is sorted";
+    else
+        cout << "Array is not sorted";
 
     return 0;
 }

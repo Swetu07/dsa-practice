0b9550c17a8aa25192d9ee004460533bcc11b6b9
Swetank Kumar <swetankkumar15f@gmail.com>
2026-09-04 22:27:02 +0530

    Finding 2nd largest/smallest element in array

--- arrayeasyl1.cpp
+++ arrayeasyl1.cpp
@@ -1,4 +1,5 @@
 // Largest element in an array
+/*
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -19,5 +20,61 @@ int main()
     }
     cout << "largest: " << largest;
     
+    return 0;
+}
+*/
+
+// 2nd smallest/largest number in the array 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter N: ";
+    cin >> n;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int largest = arr[0];
+    int Slargest = -1;
+
+    int smallest = arr[0];
+    int Ssmallest = INT_MAX;
+
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > largest)
+        {
+            Slargest = largest;
+            largest = arr[i];
+        }
+        else if (arr[i] < largest && arr[i] > Slargest)
+        {
+            Slargest = arr[i];
+        }
+    }
+
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] < smallest)
+        {
+            Ssmallest = smallest;
+            smallest = arr[i];
+        }
+        else if (arr[i] != smallest && arr[i] < Ssmallest)
+        {
+            Ssmallest = arr[i];
+        }
+    }
+
+    cout << "Second Largest : " << Slargest << endl;
+    cout << "Second Smallest : " << Ssmallest << endl;
+
     return 0;
 }

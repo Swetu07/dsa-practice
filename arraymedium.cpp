360107c699f8ca8a9e4ee27b39c2ff886282e29f
Swetank Kumar <swetankkumar15f@gmail.com>
2026-09-20 13:27:08 +0530

    Longest subarray with sum K

--- arraymedium.cpp
+++ arraymedium.cpp
@@ -1,9 +1,56 @@
 // Longest subarray with given sum K(positives)
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     int n, k;
+//     cout << "Enter size: ";
+//     cin >> n;
+
+//     vector<int> arr(n);
+
+//     cout << "Enter elements: ";
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+
+//     cout << "Enter K: ";
+//     cin >> k;
+
+//     int left = 0;
+//     int right = 0;
+//     int sum = 0;
+//     int maxLen = 0;
+
+//     while (right < n) {
+
+//         sum += arr[right];
+
+//         while (sum > k) {
+//             sum -= arr[left];
+//             left++;
+//         }
+
+//         if (sum == k) {
+//             maxLen = max(maxLen, right - left + 1);
+//         }
+
+//         right++;
+//     }
+
+//     cout << "Longest subarray length: " << maxLen << endl;
+
+//     return 0;
+// }
+
+
+// Longest subarray with sum K
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
     int n, k;
+
     cout << "Enter size: ";
     cin >> n;
 
@@ -17,25 +64,27 @@ int main() {
     cout << "Enter K: ";
     cin >> k;
 
-    int left = 0;
-    int right = 0;
+    unordered_map<int, int> mp;
+
     int sum = 0;
     int maxLen = 0;
 
-    while (right < n) {
+    for (int i = 0; i < n; i++) {
 
-        sum += arr[right];
-
-        while (sum > k) {
-            sum -= arr[left];
-            left++;
-        }
+        sum += arr[i];
 
         if (sum == k) {
-            maxLen = max(maxLen, right - left + 1);
+            maxLen = i + 1;
         }
 
-        right++;
+        if (mp.find(sum - k) != mp.end()) {
+            int len = i - mp[sum - k];
+            maxLen = max(maxLen, len);
+        }
+
+        if (mp.find(sum) == mp.end()) {
+            mp[sum] = i;
+        }
     }
 
     cout << "Longest subarray length: " << maxLen << endl;

36d49231afe72ba64d3e7237fffc04633fe51348
Swetank Kumar <swetankkumar15f@gmail.com>
2026-09-23 23:56:25 +0530

    two sum

--- arraymedium.cpp
+++ arraymedium.cpp
@@ -45,11 +45,59 @@
 
 
 // Longest subarray with sum K
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     int n, k;
+
+//     cout << "Enter size: ";
+//     cin >> n;
+
+//     vector<int> arr(n);
+
+//     cout << "Enter elements: ";
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+
+//     cout << "Enter K: ";
+//     cin >> k;
+
+//     unordered_map<int, int> mp;
+
+//     int sum = 0;
+//     int maxLen = 0;
+
+//     for (int i = 0; i < n; i++) {
+
+//         sum += arr[i];
+
+//         if (sum == k) {
+//             maxLen = i + 1;
+//         }
+
+//         if (mp.find(sum - k) != mp.end()) {
+//             int len = i - mp[sum - k];
+//             maxLen = max(maxLen, len);
+//         }
+
+//         if (mp.find(sum) == mp.end()) {
+//             mp[sum] = i;
+//         }
+//     }
+
+//     cout << "Longest subarray length: " << maxLen << endl;
+
+//     return 0;
+// }
+
+// Two Sum
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    int n, k;
+    int n, target;
 
     cout << "Enter size: ";
     cin >> n;
@@ -61,33 +109,24 @@ int main() {
         cin >> arr[i];
     }
 
-    cout << "Enter K: ";
-    cin >> k;
+    cout << "Enter target: ";
+    cin >> target;
 
     unordered_map<int, int> mp;
 
-    int sum = 0;
-    int maxLen = 0;
-
     for (int i = 0; i < n; i++) {
 
-        sum += arr[i];
+        int needed = target - arr[i];
 
-        if (sum == k) {
-            maxLen = i + 1;
+        if (mp.find(needed) != mp.end()) {
+            cout << "Indices: " << mp[needed] << " " << i << endl;
+            return 0;
         }
 
-        if (mp.find(sum - k) != mp.end()) {
-            int len = i - mp[sum - k];
-            maxLen = max(maxLen, len);
-        }
-
-        if (mp.find(sum) == mp.end()) {
-            mp[sum] = i;
-        }
+        mp[arr[i]] = i;
     }
 
-    cout << "Longest subarray length: " << maxLen << endl;
+    cout << "No pair found" << endl;
 
     return 0;
 }

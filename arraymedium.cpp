5bec21112c7253d50a6bb27c2c6c041a0effb00d
Swetank Kumar <swetankkumar15f@gmail.com>
2026-09-24 01:22:15 +0530

    Sort an array of 0's 1's & 2's

--- arraymedium.cpp
+++ arraymedium.cpp
@@ -93,40 +93,89 @@
 // }
 
 // Two Sum
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     int n, target;
+
+//     cout << "Enter size: ";
+//     cin >> n;
+
+//     vector<int> arr(n);
+
+//     cout << "Enter elements: ";
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+
+//     cout << "Enter target: ";
+//     cin >> target;
+
+//     unordered_map<int, int> mp;
+
+//     for (int i = 0; i < n; i++) {
+
+//         int needed = target - arr[i];
+
+//         if (mp.find(needed) != mp.end()) {
+//             cout << "Indices: " << mp[needed] << " " << i << endl;
+//             return 0;
+//         }
+
+//         mp[arr[i]] = i;
+//     }
+
+//     cout << "No pair found" << endl;
+
+//     return 0;
+// }
+
+
+// Sort an array of 0's 1's & 2's 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    int n, target;
+    int n;
 
     cout << "Enter size: ";
     cin >> n;
 
     vector<int> arr(n);
 
-    cout << "Enter elements: ";
+    cout << "Enter elements (only 0, 1, 2): ";
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
 
-    cout << "Enter target: ";
-    cin >> target;
+    int low = 0;
+    int mid = 0;
+    int high = n - 1;
 
-    unordered_map<int, int> mp;
+    while (mid <= high) {
 
-    for (int i = 0; i < n; i++) {
-
-        int needed = target - arr[i];
-
-        if (mp.find(needed) != mp.end()) {
-            cout << "Indices: " << mp[needed] << " " << i << endl;
-            return 0;
+        if (arr[mid] == 0) {
+            swap(arr[low], arr[mid]);
+            low++;
+            mid++;
         }
 
-        mp[arr[i]] = i;
+        else if (arr[mid] == 1) {
+            mid++;
+        }
+
+        else { 
+            swap(arr[mid], arr[high]);
+            high--;
+        }
     }
 
-    cout << "No pair found" << endl;
+    cout << "Sorted array: ";
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
 
     return 0;
 }
